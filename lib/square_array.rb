e39a27a07c3b67e4441d1426a465fcbace5bce52
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-24 22:15:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   i = 0
   squard_array = []
   while i < array.length do
-    squard_array << i ** 2 [array]
+    squard_array << i ** 2(array)
     i += 1
   end
   squard_array = []
